c5c2a07c8fd948c3f566d17421fdd52684f4222e
VIEWVIEWVIEW <39219018+VIEWVIEWVIEW@users.noreply.github.com>
2021-11-29 21:48:25 +0100

    Refactor

--- gdi3_red_black_tree/Tree.h
+++ gdi3_red_black_tree/Tree.h
@@ -10,310 +10,431 @@
 #include <cassert>
 typedef struct Node
 {
-    int key;
-    std::string value;
-    bool isRed; //1(true) = schwarz, 0(false) = rot
-    Node* left;
-    Node* right;
-    Node* parent;
+	int key;
+	std::string value;
+	bool isRed; //1(true) = schwarz, 0(false) = rot
+	Node* left;
+	Node* right;
+	Node* parent;
 } Node;
 
 typedef struct Result
 {
-    Node* Node = nullptr;
-    int index; // Index des gefundenen Elements 
-    bool success = false; // useless since we don't have any criteria for an unsuccessful Result
-    int comparisons = 0;
+	Node* Node = nullptr;
+	int index; // Index des gefundenen Elements 
+	bool success = false; // useless since we don't have any criteria for an unsuccessful Result
+	int comparisons = 0;
 } Result;
 
 class RedBlackTree
 {
 private:
-    Node* root = nullptr;
+	Node* root = nullptr;
 
-    Node* searchTreeHelper(Node* node, int key, Result* res) {
-        res->comparisons++;
-        if (node == NULL || key == node->key) {
-            return node;
-        }
+	Node* searchTreeHelper(Node* node, int key, Result* res) {
+		res->comparisons++;
+		if (node == NULL || key == node->key) {
+			return node;
+		}
 
-        if (key < node->key) {
-            return searchTreeHelper(node->left, key, res);
-        }
-        return searchTreeHelper(node->right, key, res);
-    }
+		if (key < node->key) {
+			return searchTreeHelper(node->left, key, res);
+		}
+		return searchTreeHelper(node->right, key, res);
+	}
 
-    void leftRotate(Node* x)
-    {
-        if (!x || !x->right)
-            return;
+	/*
+	* Cormen et al P. 316
+	*/
+	void leftRotate(Node* x)
+	{
+		if (!x || !x->right)
+			return;
 
-        // y stored pointer to right child of x
-        struct Node* y = x->right;
+		// y stored pointer to right child of testLeftRotation
+		Node* y = x->right;
 
-        // store y's left subtree's pointer as x's right child
-        x->right = y->left;
+		// store y's left subtree's pointer as testLeftRotation's right child
+		x->right = y->left;
 
-        // update parent pointer of x's right
-        if (x->right != nullptr)
-            x->right->parent = x;
+		// update parent pointer of testLeftRotation's right
+		if (x->right != NULL)
+			x->right->parent = x;
 
-        // update y's parent pointer
-        y->parent = x->parent;
+		// update y's parent pointer
+		y->parent = x->parent;
 
-        // if x's parent points to null => make y as root of the tree
-        if (x->parent == nullptr) {
-            root = y;
-        }
-        else if (x == x->parent->left) {
-            // replace x with y
-            x->parent->left = y;
-        }
-        else {
-            x->parent->right = y;
-        }
-        // set x as left child of y
-        y->left = x;
+		// if testLeftRotation's parent points to null => make y as root of the tree
+		if (x->parent == NULL) {
+			root = y;
+		}
+		else if (x == x->parent->left) {
+			// replace testLeftRotation with y
+			x->parent->left = y;
+		}
+		else {
+			x->parent->right = y;
+		}
+		// set testLeftRotation as left child of y
+		y->left = x;
 
-        // update parent pointer of x
-        x->parent = y;
-    };
+		// update parent pointer of testLeftRotation
+		x->parent = y;
+	};
 
-    void rotateRight(Node* y) {
-        if (!y || !y->left)
-            return;
+	void rightRotate(Node* y) {
+		if (!y || !y->left)
+			return;
 
-        struct Node* x = y->left;
-        y->left = x->right;
+		Node* x = y->left;
+		y->left = x->right;
 
-        if (x->right != nullptr)
-            x->right->parent = y;
+		if (x->right != NULL)
+			x->right->parent = y;
 
-        x->parent = y->parent;
+		x->parent = y->parent;
 
-        if (x->parent == nullptr) {
-            (root) = x;
-        }
-        else if (y == y->parent->left) {
-            y->parent->left = x;
-        }
-        else {
-            y->parent->right = x;
-        }
+		if (x->parent == NULL) {
+			root = x;
+		}
+		else if (y == y->parent->left) {
+			y->parent->left = x;
+		}
+		else {
+			y->parent->right = x;
+		}
 
-        x->right = y;
-        y->parent = x;
-    };
+		x->right = y;
+		y->parent = x;
+	};
 
-    /*
-        See Cormen et al "Introduction to Algorithms"
-        P. 318 for more information plus this typescript implementation
-        https://gist.github.com/hackwaly/7fdc89f1a0325a541f24#file-red_black_tree-ts-L122-L165
-    */
-    void fixInsert(Node* z) {
+	/*
+		See Cormen et al "Introduction to Algorithms"
+		P. 318
+	*/
+	void insertFixup(Node* z) {
 
-        // iterate till z is not the root and z's parent color is red
-        // z->parent can be be dereferencing a null pointer according to my compiler but idk lol
-        while (z != root && z != root->left && z != root->right && z->parent->isRed == 1)
-        {
-            struct Node* y;
+		// iterate till z is not the root and z's parent color is red
+		// z->parent can be be dereferencing a null pointer according to my compiler but idk lol
+		while (z->parent != nullptr && z->parent->isRed == 1)
+		{
+			Node* y;
 
-            // Find uncle and store uncle in y
-            if (z->parent && z->parent->parent && z->parent == z->parent->parent->left)
-                y = z->parent->parent->right;
-            else
-                y = z->parent->parent->left; // MSCVC: Dereferencing NULL pointer 'z->parent'. See line 130 for an earlier location where this can occur
+			if (z->parent == z->parent->parent->left) {
+				y = z->parent->parent->right;
+				if (y && y->isRed == 1) {
+					z->parent->isRed = 0;
+					y->isRed = 0;
+					z->parent->parent->isRed = 1;
+					z = z->parent->parent;
+				}
+				else if (z == z->parent->right) {
+					z = z->parent;
+					leftRotate(z);
+				}
+				else {
+					z->parent->isRed = 0;
+					z->parent->parent->isRed = 1;
+					rightRotate(z->parent->parent);
+				}
+			}
+			else {
 
+				y = z->parent->parent->left;
 
-            // If uncle is RED, do following
-            // - Change color of parent and uncle as black
-            // - Change color of grandparent as red
-            // - Move z to grandparent
-            if (!y)
-                z = z->parent->parent;
-            else if (y->isRed == 1)
-            {
-                y->isRed = 0;
-                z->parent->isRed = 0;
-                z->parent->parent->isRed = 1;
-                z = z->parent->parent;
-            }
+				if (y && y->isRed) {
+					// case 4
+					z->parent->isRed = 0;
+					y->isRed = 0;
+					z = z->parent->parent;
+				}
+				else if (z == z->parent->left) {
+					z = z->parent;
+					rightRotate(z);
+				}
+				else {
+					z->parent->isRed = 0;
+					z->parent->parent->isRed = 1;
+					leftRotate(z->parent->parent);
+				}
+			}
 
-            // Uncle is black, there are four cases (LL, LR, RL and RR)
-            else
-            {
-                // LeftLeft (LL) case, do following
-                // - swap color of parent and grandparent
-                // - rotate to the right grandparent
-                if (z->parent == z->parent->parent->left &&
-                    z == z->parent->left)
-                {
-                    bool ch = z->parent->isRed;
-                    z->parent->isRed = z->parent->parent->isRed;
-                    z->parent->parent->isRed = ch;
-                    rotateRight(z->parent->parent);
-                }
-
-                // LeftRight (LR) case, do following
-                // - swap color of current node and grandparent
-                // - left rotate parent
-                // - right rotate grand parent
-                if (z->parent && z->parent->parent && z->parent == z->parent->parent->left && z == z->parent->right)
-                {
-                    bool ch = z->isRed;
-                    z->isRed = z->parent->parent->isRed;
-                    z->parent->parent->isRed = ch;
-                    leftRotate(z->parent);
-                    rotateRight(z->parent->parent);
-                }
-
-                // RightRight (RR) case, do following
-                // - swap color of parent and grandparent
-                // - left rotate grandparent
-                if (z->parent && z->parent->parent &&
-                    z->parent == z->parent->parent->right &&
-                    z == z->parent->right)
-                {
-                    bool ch = z->parent->isRed;
-                    z->parent->isRed = z->parent->parent->isRed;
-                    z->parent->parent->isRed = ch;
-                    leftRotate(z->parent->parent);
-                }
-
-                // RightLeft (RL) case, do following
-                // - swap color of current node  and grandparent
-                // - right Rotate Parent
-                // - left Rotate Grand Parent
-                if (z->parent && z->parent->parent && z->parent == z->parent->parent->right &&
-                    z == z->parent->left)
-                {
-                    bool ch = z->isRed;
-                    z->isRed = z->parent->parent->isRed;
-                    z->parent->parent->isRed = ch;
-                    rotateRight(z->parent);
-                    leftRotate(z->parent->parent);
-                }
-            }
-        }
-        root->isRed = 0; // root is always black
-
-    };
+		}
+		root->isRed = 0; // root is always black
+	};
 
 public:
-    RedBlackTree() {
-    };
+	RedBlackTree() {
+	};
 
-    static bool test() {
-        // create tree for testing
-        RedBlackTree x = RedBlackTree();
-
-        // 3
-        x.insertKey(26, "bin root");
-        x.insertKey(17, "bin links von root und rot");
-
-        // 2
-        x.insertKey(41, "bin rechts von root und schwarz");
-        x.insertKey(21, "bin rechts von 17 und schwarz");
-        x.insertKey(14, "bin links von 17 und schwarz");
-        x.insertKey(30, "bin links von 41 und rot");
-
-        // 1
-        x.insertKey(47, "bin rechts von 41 und rot");
-        x.insertKey(23, "bin rechts von 21 und schwarz");
-        x.insertKey(19, "bin links von 21 und schwarz");
-        x.insertKey(28, "bin links von 30 und schwarz");
-        x.insertKey(38, "bin rechts von 30 und schwarz");
-        x.insertKey(35, "bin links von 38 und rot");
-        x.insertKey(39, "bin rechts von 38 und rot");
-        x.insertKey(16, "bin rechts von 14 und schwarz");
-        x.insertKey(15, "bin links von 16 und rot");
-        x.insertKey(10, "bin links von 14 und rot");
-        x.insertKey(12, "bin rechts von 10 und schwarz");
-        x.insertKey(7, "bin links von 10 und schwarz");
+	static bool test() {
+		// tests
 
 
-        // tests
-        // root node
-        assert(x.root->parent == nullptr);
-        assert(x.root->key == 26); 
-        assert(x.root->isRed == 0);
-
-        // 17
-        assert(x.root->left->key == 17); 
-        assert(x.root->left->isRed == 1);
-
-        // 41
-        assert(x.root->right->key == 41);
-        assert(x.root->right->isRed == 0);
-
-        // 14
-        assert(x.root->left->left->key == 14);
+		// ---
+		// test rotations
+		RedBlackTree testLeftRotation = RedBlackTree();
+		testLeftRotation.insertKey(7, "bin root");
+		testLeftRotation.insertKey(4, "bin links von root");
+		testLeftRotation.insertKey(11, "bin rechts von root");
+		testLeftRotation.insertKey(18, "bin rechts von 11");
+		testLeftRotation.insertKey(9, "bin links von 11");
+		testLeftRotation.insertKey(14, "bin links von 18");
+		testLeftRotation.insertKey(19, "bin rechts von 18");
 
 
-        return true;
-    }
+		// check if tree is correct. colors do not matter at this point since we are only testing rotations.
+		assert(testLeftRotation.root->key == 7);
+		assert(testLeftRotation.root->left->key == 4);
+		assert(testLeftRotation.root->right->key == 11);
+		assert(testLeftRotation.root->right->right->key == 18);
+		assert(testLeftRotation.root->right->left->key == 9);
+		assert(testLeftRotation.root->right->right->left->key == 14);
+		assert(testLeftRotation.root->right->right->right->key == 19);
 
-    Result insertKey(int key, std::string value) {
-        Result res = Result();
-        res.comparisons = 0;
+		// rotate left
+		testLeftRotation.leftRotate(testLeftRotation.root->right);
 
-        Node* z = new Node();
-        z->key = key;
-        z->value = value;
-        z->isRed = 0;
-        z->left = z->right = z->parent = nullptr;
+		// check if right rotation is correct
+		assert(testLeftRotation.root->key == 7);
+		assert(testLeftRotation.root->left->key == 4);
+		assert(testLeftRotation.root->right->left->key == 11);
+		assert(testLeftRotation.root->right->key == 18);
+		assert(testLeftRotation.root->right->left->left->key == 9);
+		assert(testLeftRotation.root->right->left->right->key == 14);
+		assert(testLeftRotation.root->right->right->key == 19);
 
-        // if root is null set our new node as root
-        if (root == nullptr)
-        {
-            z->isRed = 0;
-            root = z;
-        }
-        else
-        {
-            struct Node* y = nullptr;
-            struct Node* x = root;
+		// ---
+		// test rotate right on a new tree
+		RedBlackTree testRightRotation = RedBlackTree();
 
-            // binary search tree insert to first insert the node
-            while (x != nullptr)
-            {
-                y = x;
-                if (z->key < x->key)
-                    x = x->left;
-                else
-                    x = x->right;
-                res.comparisons++;
-            }
-            z->parent = y;
-            if (z->key > y->key)
-                y->right = z;
-            else if (z->key < y->key)
-                y->left = z;
-            else
-                y->right = z;
-            z->isRed = 1;
-            res.comparisons++;
+		testRightRotation.insertKey(10, "bin root");
+		testRightRotation.insertKey(5, "bin links von root");
+		testRightRotation.insertKey(12, "bin rechts von root");
+		testRightRotation.insertKey(2, "bin links von 5");
+		testRightRotation.insertKey(8, "bin rechts von 5");
+		testRightRotation.insertKey(6, "bin links von 8");
+		testRightRotation.insertKey(9, "bin rechts von 9");
 
-            // call fixInsert to fix RB tree property if it is voilated in newest insertion.
-            fixInsert(z);
-        }
+		// check if tree is correct. colors do not matter at this point since we are only testing rotations.
+		assert(testRightRotation.root->key == 10);
+		assert(testRightRotation.root->left->key == 5);
+		assert(testRightRotation.root->right->key == 12);
+		assert(testRightRotation.root->left->left->key == 2);
+		assert(testRightRotation.root->left->right->key == 8);
+		assert(testRightRotation.root->left->right->left->key == 6);
+		assert(testRightRotation.root->left->right->right->key == 9);
 
-        res.success = true; // useless for insert
-        res.index = z->key;
-        return res;
-    };
+		// rotate right
+		testRightRotation.rightRotate(testRightRotation.root);
 
+		// check if right rotation is correct
+		assert(testRightRotation.root->right->key == 10);
+		assert(testRightRotation.root->key == 5);
+		assert(testRightRotation.root->right->right->key == 12);
+		assert(testRightRotation.root->left->key == 2);
+		assert(testRightRotation.root->right->left->key == 8);
+		assert(testRightRotation.root->right->left->left->key == 6);
+		assert(testRightRotation.root->right->left->right->key == 9);
 
-    Result searchKey(int key) {
-        Result * res = new Result();
-        res->comparisons = 0;
-        res->index = searchTreeHelper(this->root, key, res)->key;
-        return *res; 
-    };
+		// create tree and test all axioms 
+		RedBlackTree x = RedBlackTree();
+		x.insertKey(11, "bin root");
+		x.insertKey(2, "bin links von root");
+		x.insertKey(14, "bin rechts von root");
+
+		x.insertKey(1, "bin links von 2");
+		x.insertKey(7, "bin rechts von 2");
 
 
+		x.insertKey(15, "bin rechts von 14");
 
-    
+		x.insertKey(5, "bin links von 7");
+		x.insertKey(8, "bin rechts von 7");
+
+		x.insertKey(4, "bin links von 5");
+		std::cout << "Is tree valid: " << x.isTreeValid() << std::endl;
+		return true;
+	}
+
+private: bool validation(Node* x, int* leftCounter, int* rightCounter) {
+	// funny recursive function 
+	// 1
+	assert(x);	
+
+	// 2
+	assert(root);
+
+	// 3
+	if (x->left == nullptr && x->right == nullptr && x->isRed == 0)
+		assert(x->isRed == 0);
+
+	// 4 
+	if (x->isRed) {
+		if (x->left) {
+			assert(x->left->isRed == 0);
+		}
+		if (x->right) {
+			assert(x->right->isRed == 0);
+		}
+	}
+
+	// 5
+	if (!x->isRed && x->left)
+		*leftCounter = *leftCounter + 1;
+
+	if (!x->isRed && x->right)
+		*rightCounter = *rightCounter + 1;
+
+
+	// go down to the decending nodes
+	if (x->left)
+		validation(x->left, leftCounter, rightCounter);
+	if (x->right)
+		validation(x->right, leftCounter, rightCounter);
+
+	return true;
+}
+
+private: bool isTreeValid() {
+	/*
+		1. each node is either red or black. - always true since we set it in insertKey right at the beginning
+		2. the root is black.
+		3. each leaf (nil) is black.
+		4. if a node is red, then its two children are black.
+		5. for each node, all simple paths from the node to descendant leaves contain the same number of black nodes.
+
+	*/
+
+
+	// 1 & 2 & 3 & 4 & 5
+	int leftCounter, rightCounter = 0;
+	leftCounter = 0;
+	rightCounter = 0;
+	validation(root, &leftCounter, &rightCounter);
+
+	// if the leftCounter and rightCounter are the same, we have a correct rule #5
+	assert(leftCounter == rightCounter);
+	// std::cout << "left: " << leftCounter << " right: " << rightCounter << std::endl;
+
+	return true;
+}
+
+public:    Result insertKey(int key, std::string value) {
+	Result res = Result();
+	res.comparisons = 0;
+
+	Node* y = NULL;
+	Node* x = root;
+
+	Node* z = new Node();
+	z->key = key;
+	z->value = value;
+	z->isRed = 0;
+	z->left = z->right = z->parent = nullptr;
+
+	while (x != NULL) {
+		y = x;
+		res.comparisons++;
+		if (z->key < x->key) {
+			x = x->left;
+		}
+		else {
+			x = x->right;
+		}
+	}
+
+	z->parent = y;
+
+	res.comparisons++;
+	if (y == nullptr) {
+		root = z;
+	}
+	else if (z->key < y->key) {
+		y->left = z;
+	}
+	else {
+		y->right = z;
+	}
+
+	z->left = nullptr;
+	z->right = nullptr;
+	z->isRed = 1;
+	insertFixup(z);
+
+	return res;
+
+	/*
+	Result res = Result();
+	res.comparisons = 0;
+
+	Node* z = new Node();
+	z->key = key;
+	z->value = value;
+	z->isRed = 1;
+	z->left = z->right = z->parent = nullptr;
+
+	// if root is null set our new node as root
+	if (root == nullptr)
+	{
+		z->isRed = 0;
+		root = z;
+		res.success = true;
+		res.index = z->key;
+		res.comparisons++;
+		return res;
+	}
+	else
+	{
+		Node* y = nullptr;
+		Node* x = root;
+
+		// binary search tree insert to first insert the node
+		while (x != nullptr)
+		{
+			y = x;
+			if (z->key < x->key) {
+				x = x->left;
+			}
+			else {
+				x = x->right;
+			}
+			res.comparisons++;
+		}
+		z->parent = y;
+		if (y == nullptr) {
+			root = z;
+		}
+		else if (z->key < y->key) {
+			y->left = z;
+		}
+		else {
+			y->right = z;
+		}
+		z->isRed = 1;
+		res.comparisons++;
+
+		// call insertFixup to fix RB tree property if it is voilated in newest insertion.
+		insertFixup(z);
+	}
+
+	res.success = true;
+	res.index = z->key;
+	return res;
+	*/
+};
+
+
+	  Result searchKey(int key) {
+		  Result* res = new Result();
+		  res->comparisons = 0;
+		  res->index = searchTreeHelper(this->root, key, res)->key;
+		  return *res;
+	  };
+
+
+
+
 
 };
 
