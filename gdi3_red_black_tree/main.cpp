cc4a803250d427db76ddb78b618f6d792754da10
VIEWVIEWVIEW <39219018+VIEWVIEWVIEW@users.noreply.github.com>
2021-12-02 17:52:18 +0100

    Added example

--- gdi3_red_black_tree/main.cpp
+++ gdi3_red_black_tree/main.cpp
@@ -1,21 +1,24 @@
-// gdi3_red_black_tree.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "Tree.h"
+
+// Beispiel ist ein Beispiel zur Benutzung der Library.
+void Beispiel() {
+    RedBlackTree exampleTree = RedBlackTree();
+    exampleTree.insertKey(7, "bin root");
+    exampleTree.insertKey(4, "bin links von root");
+    exampleTree.insertKey(11, "bin rechts von root");
+
+    Result res = exampleTree.searchKey(11);
+    std::cout << "Success " << res.success << std::endl;
+    std::cout << "Comparisons " << res.comparisons << std::endl;
+    Node* node = res.Node;
+}
 
 int main()
 {
     RedBlackTree::test();
+
+    Beispiel();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
